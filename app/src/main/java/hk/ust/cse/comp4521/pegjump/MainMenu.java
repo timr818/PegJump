82a5e0074a6ace20a8ad033191ac6b1e54a740fd
Tim Regan <tjrf2c@mst.edu>
2018-03-28 20:14:16 +0800

    initial commit

---
+++ app/src/main/java/hk/ust/cse/comp4521/pegjump/MainMenu.java
@@ -0,0 +1,13 @@
+package hk.ust.cse.comp4521.pegjump;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
