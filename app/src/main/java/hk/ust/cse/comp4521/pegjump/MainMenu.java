71e7dabbe3f642ffc3413160a643235bc6fc2887
Tim Regan <tjrf2c@mst.edu>
2018-03-29 15:19:06 +0800

    added activity navigation

--- app/src/main/java/hk/ust/cse/comp4521/pegjump/MainMenu.java
+++ app/src/main/java/hk/ust/cse/comp4521/pegjump/MainMenu.java
@@ -1,7 +1,10 @@
 package hk.ust.cse.comp4521.pegjump;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        configureButtons();
+    }
+
+    private void configureButtons() {
+        Button playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenu.this, PlayGame.class));
+            }
+        });
+
+        Button settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenu.this, Settings.class));
+            }
+        });
     }
 }
